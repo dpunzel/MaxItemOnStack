eb4b5764b7d99a281ad3a03038c467238b1de65b
DAVID <dpunzel15@georgefox.edu>
2019-10-17 22:52:14 -0700

    Initial commit

---
+++ src/maxItemStack/App.java
@@ -0,0 +1,4 @@
+package maxItemStack;
+
+public class App {
+}
---
+++ src/maxItemStack/MaxItemStack.java
@@ -0,0 +1,33 @@
+package maxItemStack;
+
+import java.util.Stack;
+
+public class MaxItemStack {
+
+    private Stack<Integer> maxStack;
+    private Stack<Integer> mainStack;
+
+    public MaxItemStack() {
+        this.mainStack = new Stack<>();
+        this.maxStack = new Stack<>();
+    }
+
+    public void push(int item) {
+        // push the new item onto the stack
+        mainStack.push(item);
+
+        // first item is the same in both stacks
+        if (mainStack.size() == 1) {
+            maxStack.push(item);
+            return;
+        }
+
+        // if the item is the largest one so far, insert to stack
+        if (item > maxStack.peek()) {
+            maxStack.push(item);
+        } else {
+            // if not largest, duplicate the largest on maxStack
+            maxStack.push(maxStack.peek());
+        }
+    }
+}
